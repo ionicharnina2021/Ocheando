74abc2069f69f74799ffe38c864cae2612e655a0
programacion <>
2021-10-01 12:25:10 +0200

    ejercicio 02

--- src/ejercicioStrema03/Ejercicio02.java
+++ src/ejercicioStrema03/Ejercicio02.java
@@ -1,6 +1,9 @@
 package ejercicioStrema03;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Ejercicio02 {
 	ArrayList<Vehiculo> vehiculos = new ArrayList<>();
@@ -16,8 +19,21 @@ public class Ejercicio02 {
 		vehiculos.add(new Vehiculo("6666FFF", Modelo.OPEL, 300000));
 		vehiculos.add(new Vehiculo("7777GGG", Modelo.CITROEN, 0));
 	}
-	public static void main(String[] args) {
-
+	public List<Vehiculo> alteraCollecion(){
+		//separando la funcion para verlo mejor
+//		Function<Vehiculo, Vehiculo> mapeo = new Function<Vehiculo, Vehiculo>() {
+//
+//			@Override
+//			public Vehiculo apply(Vehiculo t) {
+//				t.setKilometros(t.getKilometros() * 2);
+//				return t;
+//			}
+//		};
+//		vehiculos.stream().map(mapeo).collect(Collectors.toList());
+		return vehiculos.stream().map((v) -> {
+			v.setKilometros(v.getKilometros() * 2);
+			return v;
+		}).collect(Collectors.toList());
+		
 	}
-
 }
---
+++ src/ejercicioStrema03/Ejercicio02Test.java
@@ -0,0 +1,24 @@
+package ejercicioStrema03;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class Ejercicio02Test {
+
+	@Test
+	void test() {
+		Ejercicio02 exercise = new Ejercicio02();
+		Integer[] expected= {20000, 40000, 60000, 200000, 400000, 600000, 0};
+		List<Vehiculo> alteraCollecion = exercise.alteraCollecion();
+		ArrayList<Integer> kilometrajes=new ArrayList<>();
+		alteraCollecion.stream().forEach((v)->{
+			kilometrajes.add(v.getKilometros());
+		});
+		assertArrayEquals(expected, kilometrajes.toArray());
+	}
+
+}
