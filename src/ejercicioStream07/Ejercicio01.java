e0bc415b510973010fdf732e873bdcd226740a04
programacion <>
2022-10-07 08:23:48 +0200

    chino 1

---
+++ src/ejercicioStream07/Ejercicio01.java
@@ -0,0 +1,31 @@
+package ejercicioStream07;
+
+
+import java.util.*;
+public class Ejercicio01
+{
+//	1. Genere 10 números aleatorios que van del 1 al 100
+//	y colóquelos en una matriz Coloque los números mayores
+//	o iguales a 10 en la matriz 
+//	en un conjunto de listas e imprímalos en la consola.
+	public static void main(String[] args)
+	{
+		ArrayList al = new ArrayList();
+		 int [] arr = new int [10]; // Indica la longitud
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = (int)(Math.random()*100+1);
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]>=10)
+				al.add(arr[i]);
+		}
+		Iterator it = al.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+	}
+}
+
