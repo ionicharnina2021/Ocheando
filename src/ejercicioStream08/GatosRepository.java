782327d9c078cf51efb80d3c15a402690f97bea8
programacion <>
2022-10-10 09:33:12 +0200

    ejemplo gatos

--- src/ejercicioStream08/GatosRepository.java
+++ src/ejercicioStream08/GatosRepository.java
@@ -2,6 +2,7 @@ package ejercicioStream08;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class GatosRepository {
 	private Set<Gato> gatos=new HashSet<>();
@@ -10,6 +11,16 @@ public class GatosRepository {
 		return gatos.add(e);
 	}
 
+	public void ejemplo() {
+		//para hacer depuracion
+		Stream cc=gatos.stream().filter(()->);
+		System.out.println();
+		cc.map().filter().collect();
+		//para hacer log
+		gatos.stream().filter(()->)
+		.peek((obj)->{syso})		
+		.map().filter().collect();
+	}
 	public int size() {
 		return gatos.size();
 	}
